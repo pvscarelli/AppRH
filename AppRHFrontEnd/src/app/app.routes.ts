b57ea1973343fab27f4d1fd9dfbcad3cea168f2f
Pedro Scarelli <pedro.virelli@gmail.com>
2024-05-09 08:30:49 -0300

    etst

--- AppRHFrontEnd/src/app/app.routes.ts
+++ AppRHFrontEnd/src/app/app.routes.ts
@@ -3,11 +3,25 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { ListjobsComponent } from './components/pages/listjobs/listjobs.component';
 import { JobdetailsComponent } from './components/pages/jobdetails/jobdetails.component';
 import { JobformComponent } from './components/pages/jobform/jobform.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { LoginLayoutComponent } from './components/login-layout/login-layout.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'register_job', component: JobformComponent },
-  { path: 'jobs', component: ListjobsComponent },
-  { path: 'update_job/:id', component: JobformComponent },
-  { path: 'job_details/:id', component: JobdetailsComponent },
+  {
+    path: '',
+    component: LoginLayoutComponent,
+    children: [{ path: '', component: LoginComponent }],
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'register_job', component: JobformComponent },
+      { path: 'jobs', component: ListjobsComponent },
+      { path: 'update_job/:id', component: JobformComponent },
+      { path: 'job_details/:id', component: JobdetailsComponent },
+    ],
+  },
 ];
---
+++ AppRHFrontEnd/src/app/components/layout/layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+})
+export class LayoutComponent {}
